8b9ec5e16bf2ee55caf6ba30dce2f8b737f1793a
Allyson Belli Bogo <ab.bogo@gmail.com>
2023-02-06 15:13:18 -0300

    Requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,81 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    isLoading: false,
+    favoriteSongs: [],
+  };
+
+  async componentDidMount() {
+    this.setState({
+      favoriteSongs: await getFavoriteSongs(),
+    });
+  }
+
+  isFavorite = (song) => {
+    const { favoriteSongs } = this.state;
+    let validation = false;
+    if (favoriteSongs
+      .some((favoriteSong) => favoriteSong.trackId === song.trackId)) validation = true;
+    return validation;
+  };
+
+  handleChange = (song) => {
+    this.setState({
+      isLoading: true,
+    }, async () => {
+      if (this.isFavorite(song)) removeSong(song);
+      if (!this.isFavorite(song)) addSong(song);
+      this.setState({
+        favoriteSongs: await getFavoriteSongs(),
+        isLoading: false,
+      });
+    });
+  };
+
   render() {
+    const { isLoading, favoriteSongs } = this.state;
+
+    if (isLoading) return <Loading />;
+
     return (
       <div data-testid="page-favorites">
         <Header />
+
+        {
+          favoriteSongs.map((song) => (
+            <div key={ song.trackId }>
+              <p>{ song.trackName }</p>
+
+              <audio data-testid="audio-component" src={ song.previewUrl } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                {' '}
+                <code>audio</code>
+                .
+              </audio>
+
+              <label
+                htmlFor={ `favorite-music-${song.trackId}` }
+                data-testid={ `checkbox-music-${song.trackId}` }
+              >
+                Favorita
+                <input
+                  id={ `favorite-music-${song.trackId}` }
+                  type="checkbox"
+                  name="favorite-input"
+                  checked={ favoriteSongs
+                    .some((favoriteSong) => favoriteSong.trackId === song.trackId) }
+                  onChange={ () => this.handleChange(song) }
+                />
+              </label>
+            </div>
+          ))
+        }
       </div>
     );
   }
